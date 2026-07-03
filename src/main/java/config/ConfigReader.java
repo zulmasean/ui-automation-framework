a14b9f0a3eb2f880b80a2c865886d502c997f5c0
zulma <m@MacBook-Air-macbook.local>
2026-07-03 09:48:52 +0700

    config

--- src/main/java/config/ConfigReader.java
+++ src/main/java/config/ConfigReader.java
@@ -2,11 +2,13 @@ package config;
 import java.io.*;import java.util.*;
 public class ConfigReader{
 Properties p=new Properties();
-public ConfigReader(){
-try{
-FileInputStream f=new FileInputStream("src/main/resources/config.properties");
-p.load(f);
-}catch(Exception e){e.printStackTrace();}
+    public ConfigReader(){
+        try{
+            FileInputStream f=new FileInputStream("src/main/resources/config.properties");
+            p.load(f);
+        }catch(Exception e){e.printStackTrace();}
 }
-public String get(String k){return p.getProperty(k);}
+    public String get(String k){
+        return p.getProperty(k);
+    }
 }
--- src/main/java/drivers/DriverManager.java
+++ src/main/java/drivers/DriverManager.java
@@ -2,17 +2,42 @@ package drivers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-public class DriverManager{
-static WebDriver driver;
-public static WebDriver getDriver(){
-if(driver==null){
-WebDriverManager.chromedriver().setup();
-driver=new ChromeDriver();
-driver.manage().window().maximize();
-}
-return driver;
-}
-public static void quitDriver(){
-if(driver!=null){driver.quit();driver=null;}
-}
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverManager {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // wajib untuk GitHub Actions/Linux
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--remote-allow-origins=*");
+
+            // optional
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
+
+            driver.manage().window().maximize();
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
